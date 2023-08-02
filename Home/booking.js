96d8b5d78a90300e5888a5809ec8aae0f51f5bb9
Bich Tracy <ethan.chan.yong.guan@gmail.com>
2023-08-03 00:43:45 +0800

    m

--- Home/booking.js
+++ Home/booking.js
@@ -42,5 +42,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const calendar2 = new VanillaCalendar('#calendar-input2', options2);
   calendar2.init();
+
+  const submitButton = document.getElementById('submitbutton');
+
+  submitButton.addEventListener('click', () => {
+    let calendar1val = document.getElementById("calendar-input").value;
+    let calendar2val = document.getElementById("calendar-input2").value;
+    let numAdult = document.getElementById("adults").value;
+    let numChildren = document.getElementById('children').value;
+    let hotelRadios = document.getElementsByName('room_type');
+    let hotelSelect = Array.from(hotelRadios).find((radio) => {
+      return radio.checked;
+    }).value;
+    
+    if (calendar1val && calendar2val && numAdult && hotelSelect) {
+      if (numChildren == 0 || numChildren ) {
+        txt = window.alert('Thank you for your submission');
+        document.getElementById("calendar-input").value = '';
+        document.getElementById("calendar-input2").value = '';
+        Array.from(hotelRadios)[0].checked = true;
+      }
+    } else {
+      window.alert('Please fill in the required fields.')
+    }
+
+  })
 });
 
